051a1a5341f526b18c43c6983e555d7f9c0f105e
StaRa1n <1945159551@qq.com>
2023-09-01 22:27:35 +0800

    feat: code

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,13 +1,7 @@
 <template>
-  <div class="container">
-    
-  </div>
+  <div class="container"></div>
 </template>
 
-<script setup lang="ts">
+<script setup lang="ts"></script>
 
-</script>
-
-<style scoped>
-
-</style>
+<style scoped></style>
