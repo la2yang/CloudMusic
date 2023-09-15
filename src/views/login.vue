cb4424f0a7a0a5ca7c7efb67b048e3958900d421
StaRa1n <1945159551@qq.com>
2023-09-15 23:52:14 +0800

    feat: 二维码登录完善

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,4 +1,5 @@
 <template>
+  <!-- 二维码登录界面 -->
   <div class="login">
     <div class="logo">
       <img src="@/assets/images/avatar.png" />
@@ -7,82 +8,99 @@
     <div class="qrcode" @click="getQrCode()">
       <img :src="loginData.qrCodeSvg" />
     </div>
-    <p>打开网易云音乐app扫码登录</p>
+    <button @click="setUserInfo()">刷新二维码</button>
+    <p>{{ loginData.qrCodeInformation }}</p>
   </div>
 </template>
 
 <script setup lang="ts">
+import { onBeforeUnmount, onMounted, reactive } from 'vue'
+import Router from '@/router'
 import QRCode from 'qrcode'
+import {
+  loginQrCodeKey,
+  loginQrCodeCheck,
+  getLoginStatus,
+  getUserDetail,
+} from '@/api/user'
+import useUserStore from '@/store/user'
 
-import { loginQrCodeKey, loginQrCodeCheck } from '@/api/login'
-import { onMounted, reactive } from 'vue'
+const userStore = useUserStore()
 
+// 登录验证状态
 const loginData = reactive({
   qrCodeKey: '',
   qrCodeSvg: '',
-  qrCodeInformation: '',
+  qrCodeInformation: '请扫码',
   qrCodeCheckInterval: null,
 })
 
-// onMounted(() => {
-//   getQrCode()
-// })
+onMounted(() => {
+  getQrCode()
+  checkQrCodeLogin()
+})
 
-const getQrCode = () => {
-  console.log(loginData)
-
-  return loginQrCodeKey().then((result: any) => {
-    if (result.code === 200) {
-      loginData.qrCodeKey = result.data.unikey
-      console.log(loginData)
-
-      QRCode.toString(
-        `https://music.163.com/login?codekey=${loginData.qrCodeKey}`,
-        {
-          width: 192,
-          margin: 0,
-          color: {
-            dark: '#33a970',
-            light: '#00000000',
-          },
-          type: 'svg',
+// 获取二维码key并转化图像
+const getQrCode = async () => {
+  const result: any = await loginQrCodeKey()
+  if (result.code === 200) {
+    loginData.qrCodeKey = result.data.unikey
+    QRCode.toString(
+      `https://music.163.com/login?codekey=${loginData.qrCodeKey}`,
+      {
+        width: 192,
+        margin: 0,
+        color: {
+          dark: '#33a970',
+          light: '#00000000',
         },
-      )
-        .then((svg) => {
-          loginData.qrCodeSvg = `data:image/svg+xml;utf8,${encodeURIComponent(
-            svg,
-          )}`
-          console.log(loginData)
-        })
-        .catch((err) => {
-          console.error(err)
-        })
-    }
-  })
+        type: 'svg',
+      },
+    )
+      .then((svg) => {
+        loginData.qrCodeSvg = `data:image/svg+xml;utf8,${encodeURIComponent(
+          svg,
+        )}`
+      })
+      .catch((err) => {
+        console.error(err)
+      })
+  }
 }
 
-const checkQrCodeLogin = () => {
-  // 清除二维码检测
+// 二维码状态检测
+const checkQrCodeLogin = async () => {
   clearInterval(loginData.qrCodeCheckInterval)
-  loginData.qrCodeCheckInterval = setInterval(() => {
+  loginData.qrCodeCheckInterval = setInterval(async () => {
     if (loginData.qrCodeKey === '') return
-    loginQrCodeCheck(loginData.qrCodeKey).then((result: any) => {
-      if (result.code === 800) {
-        getQrCode() // 重新生成QrCode
-        loginData.qrCodeInformation = '二维码已失效，请重新扫码'
-      } else if (result.code === 802) {
-        loginData.qrCodeInformation = '扫描成功，请在手机上确认登录'
-      } else if (result.code === 801) {
-        loginData.qrCodeInformation = '打开网易云音乐APP扫码登录'
-      } else if (result.code === 803) {
-        clearInterval(loginData.qrCodeCheckInterval)
-        loginData.qrCodeInformation = '登录成功，请稍等...'
-        result.code = 200
-        result.cookie = result.cookie.replaceAll(' HTTPOnly', '')
-      }
-    })
+    const result: any = await loginQrCodeCheck(loginData.qrCodeKey)
+    if (result.code === 800) {
+      getQrCode() // 重新生成QrCode
+      loginData.qrCodeInformation = '二维码已失效，请重新扫码'
+    } else if (result.code === 802) {
+      loginData.qrCodeInformation = '扫描成功，请在手机上确认登录'
+    } else if (result.code === 801) {
+      loginData.qrCodeInformation = '打开网易云音乐APP扫码登录'
+    } else if (result.code === 803) {
+      clearInterval(loginData.qrCodeCheckInterval)
+      loginData.qrCodeInformation = '登录成功，请稍等...'
+      localStorage.setItem('cookie', result.cookie)
+      setUserInfo()
+      Router.push('/library')
+    }
   }, 1000)
 }
+
+// 本地存储cookie
+const setUserInfo = async () => {
+  const result = await getLoginStatus()
+  localStorage.setItem('user', JSON.stringify(result.data.profile))
+  userStore.setUserInfo(result.data.profile)
+}
+// 清除校验二维码定时器
+onBeforeUnmount(() => {
+  clearInterval(loginData.qrCodeCheckInterval)
+})
 </script>
 
 <style scoped lang="scss">
@@ -117,3 +135,4 @@ const checkQrCodeLogin = () => {
   background-color: $block-hover;
 }
 </style>
+@/api/user
